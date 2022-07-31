8e2f9438e2b5b7e9f8840432a72fbd5f57c33d5c
BigBadaBoom <you@example.com>
2022-07-31 16:17:19 +0300

    Створив діалог

--- lesson_3/dialog.py
+++ lesson_3/dialog.py
@@ -1,1 +1,12 @@
 # my homework 1
+print("Привіт, мене звати Саша!")
+who = input("Як тебе звати?")
+print("Вітаю," + who + '!')
+number_1 = input(who + ", загадай число:")
+a = int(number_1)
+number_2 = input("Загадай інше число:")
+b = int(number_2)
+print("Дякую. Я все склав і вийшло:")
+print(a + b)
+
+
