4f4ac976789a246d5a431a5ad459f09e07e3c965
BigBadaBoom <you@example.com>
2022-08-03 22:41:51 +0300

    Виконав друге завдання

---
+++ lesson_4/parti_i_ychni.py
@@ -0,0 +1,16 @@
+# my home work 2
+
+print("Вирішуємо задачу №2")
+klas_a = input("Скільки учнів в класі А?")
+a = int(klas_a)
+klas_b = input("Скільки учнів в класі Б?")
+b = int(klas_b)
+klas_v = input("Скільки учнів в класі В?")
+v = int(klas_v)
+print("Всього учнів:")
+c = (a + b + v)
+print(c)
+x = (c % 2)
+print("Всього потрібно парт:")
+print ((c + x) // 2)
+
