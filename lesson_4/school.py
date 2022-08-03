f50b66a86cfe3d0498a6026af4f9460522ae54ff
BigBadaBoom <you@example.com>
2022-08-03 21:58:28 +0300

    Виконав перше завдання

--- lesson_4/school.py
+++ lesson_4/school.py
@@ -1,1 +1,14 @@
 # my home work
+
+print("Вирішуємо задачу №1")
+children = input("Скільки школярів?")
+n = int(children)
+apple = input("Скільки яблук?")
+k = int(apple)
+print("Кожному школяру дісталося яблук")
+print(k // n)
+print("Яблук залишилось в корзинці")
+print(k % n)
+
+
+
