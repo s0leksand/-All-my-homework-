bf029e9ca9b79bf61f2e1259e9b7ec209d4868d7
BigBadaBoom <you@example.com>
2022-08-04 13:44:02 +0300

    Доробив 3 завдання

---
+++ lesson_4/password_new.py
@@ -0,0 +1,13 @@
+# my home work 3
+
+pas = input("Введіть пароль:")
+if pas == "R2d2":
+    print("пароль вірний!")
+elif pas == "dron7":
+    print("пароль вірний!")
+elif pas == "qwerty":
+    print("пароль вірний!")
+elif pas == "123456":
+    print("пароль вірний!")
+else:
+    print("Нажаль пароль не вірний!")
