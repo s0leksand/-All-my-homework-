99f582fbd16d022fd1a87ec2f2095074e8bfb2c3
BigBadaBoom <you@example.com>
2022-08-18 02:29:25 +0300

    1 завдання

---
+++ lesson_7/task_1.py
@@ -0,0 +1,6 @@
+x = list(input("Введіть числа: "))
+y = int(input("Введіть індекс числа: "))
+x[y]=[]
+x.remove([])
+x.pop()
+print(x)
---
+++ lesson_9/lesson_9.py
@@ -0,0 +1,47 @@
+#d = {}
+#d1 = dict()
+#print(d, d1)
+
+
+#dict_kv = {"sm": 10, "m": 1000, "coord": (23, 45)}
+#print(dict_kv['sm'])
+#print(len(dict_kv))
+
+
+#print("mm in m =", dict_kv['m'])
+#dict_kv['coord'] = (0, 0)
+#print(dict_kv)
+
+#d = dict(m=2, b=3, c=34)
+#print(d)
+
+#n = dict(max=(123, 34), min=(342))
+#print(n["max"])
+
+#d = dict({1: 3, 3: 3333}, max=12)
+#print(d)
+
+
+#dict_kv = {"sm": 10, "m": 1000, "coord": (23, 45)}
+#for i in dict_kv:
+    #print(i, "=", dict_kv[i])
+
+#print(dict_kv.get('sm', "тут нема нічого"))
+
+
+#internal_d = {"eage": 12, "height": 170}
+#external_d = {"Ivan": internal_d}
+#print(external_d)
+#class_a = {"Ivan": {
+# "scores": [4,6,7,8],
+# "second_name": "Petrov"
+#    },
+#"Mikola": {
+#  "scores": [5,3,2,4],
+#  "second_name": "Lom"
+#    }
+
+#}
+#print(class_a["Mikola"]["scores"][2])
+
+{k: v}
