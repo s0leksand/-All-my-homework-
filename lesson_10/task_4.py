04d2c3196560a2e6e17d55c05097095b2416ea19
BigBadaBoom <you@example.com>
2022-09-12 20:23:47 +0300

    почистив помилки

--- lesson_10/task_4.py
+++ lesson_10/task_4.py
@@ -8,5 +8,4 @@ sick = {i for i in range(35, 35 + 3)}
 gym = {i for i in range(1, 6)}
 circus = {i for i in range(6, 6 + 10)}
 
-
 print(len((planet | circus | sick | gym) - (planet_circus | planet_gym | gym_circus)))
