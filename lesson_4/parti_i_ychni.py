eceb813305c071f75e05bfc96e56a44cd86b3dc9
BigBadaBoom <you@example.com>
2022-10-04 22:03:13 +0300

    Доопрацював друге завдання

--- lesson_4/parti_i_ychni.py
+++ lesson_4/parti_i_ychni.py
@@ -1,16 +1,10 @@
 # my home work 2
 
-print("Вирішуємо задачу №2")
-klas_a = input("Скільки учнів в класі А?")
-a = int(klas_a)
-klas_b = input("Скільки учнів в класі Б?")
-b = int(klas_b)
-klas_v = input("Скільки учнів в класі В?")
-v = int(klas_v)
-print("Всього учнів:")
-c = (a + b + v)
-print(c)
-x = (c % 2)
-print("Всього потрібно парт:")
-print ((c + x) // 2)
+f = int(input("Учнів в першому класі: "))
+s = int(input("Учнів в другому класі: "))
+t = int(input("Учнів  в третьому класі: "))
+
+desk = f // 2 + f % 2 + s // 2 + s % 2 + t // 2 + t % 2
+
+print("Необхідна кількість столів :", desk)
 
