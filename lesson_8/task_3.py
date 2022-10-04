161521216b53200c6fad414c9c81e2ced52ebebb
BigBadaBoom <you@example.com>
2022-10-04 20:45:35 +0300

    фініш 3 завдання

--- lesson_8/task_3.py
+++ lesson_8/task_3.py
@@ -1,17 +1,17 @@
 import random
 
-sum_even = 0
-sum_odd = 0
+x = 0
+y = 0
 
 arr = [random.randint(1, 20) for j in range(15)]
 
 for i in range(15):
     if arr[i] % 2 == 0:
-        sum_even += arr[i]
+        x += arr[i]
     else:
-        sum_odd += arr[i]
+        y += arr[i]
 
-if sum_even < sum_odd:
-    print("Yes")
+if x < y:
+    print("Так")
 else:
-    print("No")
+    print("Ні")
